67e0a49c73fcb4961b4cf8aef0de0753ff8a4e65
pratiknawale93 <pratiknawale93@gmail.com>
2026-09-23 23:12:05 +0530

    Add files via upload

---
+++ code.py
@@ -0,0 +1,26 @@
+'''class name:
+    name1="Pratik"
+    name2="rajesh"
+
+n1=name()
+print(n1.name1)'''
+
+'''
+text=input("Enter the value of String : ")
+
+seen=set()
+duplicate=set()
+count=0
+
+for ch in text:
+    if ch in seen:
+        duplicate.add(ch)
+    else:
+        seen.add(ch)
+        count+=1
+print(f"The unique count of elements : {count} and the char are {seen}")  '''
+
+
+
+
+
